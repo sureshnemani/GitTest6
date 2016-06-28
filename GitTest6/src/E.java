d8ab7f5622d54bac5062c062741c0661e24f8f6c
Suresh Nemani <chandra.nemani@gmail.com>
2016-06-28 14:34:25 +0530

    "Branch 2 added and changed code in the eclipse"

--- GitTest6/src/E.java
+++ GitTest6/src/E.java
@@ -4,6 +4,7 @@ public class E {
 	public static void main(String[] args) {
 		System.out.println("E class");
 		System.out.println("Branch1 Added from Eclipse");
+		System.out.println("Branch 2 added from Remorte");
 
 	}
 
